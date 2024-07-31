1f577aa91b571aaaf7c5bf7ebcd6e606b35a5273
Thilaga-26 <112231561+Thilaga-26@users.noreply.github.com>
2024-07-31 13:57:35 +0530

    vyos-1x

---
+++ example2.py
@@ -0,0 +1,10 @@
+def add_numbers(a,b): return a+b
+
+def greet(name): print(f"Hello, {name}!")
+
+def main():
+  result = add_numbers(5,3)
+  print(f"The result is: {result}")
+  greet("Alice")
+
+if __name__ == "__main__": main()
