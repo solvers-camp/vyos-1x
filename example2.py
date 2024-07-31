79fe9071591c59bbf7be2e885993d62858020b7f
Thilaga Dinesh <thilo08071995@gmail.com>
2024-07-31 14:15:32 +0530

    Update example2.py

--- example2.py
+++ example2.py
@@ -3,7 +3,7 @@ def add_numbers(a,b): return a+b
 def greet(name): print(f"Hello, {name}!")
 
 def main():
-  result = add_numbers(5,3)
+  result = add_numbers(5,3 )
   print(f"The result is: {result}")
   greet("Alice")
 
