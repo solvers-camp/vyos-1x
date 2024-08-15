88f21f3bd143b8beaf63b5c24a2f8ef0f6ac1d6f
Thilaga Dinesh <thilo08071995@gmail.com>
2024-08-15 17:14:52 +0530

    Update testcase7.py
    (cherry picked from commit 9d70154483a139a7943bf146135663f5fa902a0d)
    
    # Conflicts:
    #	testcase7.py

--- testcase7.py
+++ testcase7.py
@@ -4,4 +4,8 @@ def add_numbers(a, b):
 
 if __name__ == "__main__":
     result = add_numbers(3, 5)
+<<<<<<< HEAD
     print(f"This is result of adding 3 and 5 is {result}")
+=======
+    print(f"This result of adding 3 and 5 is {result}")
+>>>>>>> 9d7015448 (Update testcase7.py)
