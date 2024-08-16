cae20ded5bfc5e8bb85c7551fc9a8c0f7d9915de
Thilaga Dinesh <thilo08071995@gmail.com>
2024-08-16 14:13:59 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,8 +1,8 @@
 # calculator.py
 
 class Calculator:
-    def add(self, a, b):
-        return a + b
+    def add(self, a, b, c):
+        return a + b + c
 
     def subtract(self, a, b):
         return a - b
