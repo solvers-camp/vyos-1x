e4799ee63bc31f25d3181fb3eb5bbd00f794f241
vijaikannangit <128213310+vijaikannangit@users.noreply.github.com>
2024-08-27 18:13:32 +0530

    T8888: Update test1.py

--- test1.py
+++ test1.py
@@ -9,5 +9,6 @@ def hello_world():
     print("Hello, world!")# No space - formating check
     print("Hello, world!") # Indentation Check
     print("Hello, world!")# No space - 2 formating check
-
+    print("Hello, world!")# No space - 3 formating check
+    
 hello_world()
