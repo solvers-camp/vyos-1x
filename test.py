0909ec22560bea77fbd2c6cef85850e860492646
vijaikannangit <128213310+vijaikannangit@users.noreply.github.com>
2024-08-27 15:02:30 +0530

    T003 : Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import date
 import datetime
 
 def hello_world():
-  print("Hello, world!") # Incorrect indentation
+  print("Hello, world!")
   print("Test Case1") # Incorrect indentation
   print("Another line")# Missing space before comment
   print("Another one more line")# Missing space before comment
