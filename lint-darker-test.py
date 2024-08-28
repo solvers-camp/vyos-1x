a6c45e6529390a9fe8fc2e245ffa2207a83fa0a0
Vijaikannan <vijaik.nd@gmail.com>
2024-08-28 13:58:00 +0530

    Darker Test

---
+++ lint-darker-test.py
@@ -0,0 +1,22 @@
+import os, sys, datetime
+
+def example_function():
+  print("This is an example function with formatting and linting issues")
+  if True:
+      print("This line is indented correctly")
+  else:
+    print("This line is not indented correctly")
+  print("This line has trailing whitespace ")
+  print("This line has a long string that exceeds the maximum line length limit which is usually set to 79 or 99 characters depending on the style guide being followed in the project")
+  unused_variable = 42
+
+def another_function():
+    print("Another function")
+    if True:
+        print("This line is fine")
+    else:
+      print("This line is not fine")
+    print("Another line with trailing whitespace ")
+
+example_function()
+another_function()
