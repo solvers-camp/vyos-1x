f666d70798510bc1415f9db810c8484ee0b9cc28
vijaikannangit <128213310+vijaikannangit@users.noreply.github.com>
2024-08-28 17:11:09 +0530

    T7979: Darker-Test

--- lint-darker-test.py
+++ lint-darker-test.py
@@ -1,5 +1,4 @@
 import os, sys, datetime, date, requests
-import re, csv, request
 
 def example_function():
   print("This is an example function with formatting and linting issues")
@@ -10,7 +9,6 @@ def example_function():
     print("This line has trailing whitespace ")
     print("This line has a long string that exceeds the maximum line length limit which is usually set to 79 or 99 characters depending on the style guide being followed in the project")
     unused_variable = 42
-    unused_variable2 = 22
 
 def another_function():
     print("Another function")
