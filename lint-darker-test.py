17a0369da9e326731d66f08e4773ef474f41a064
vijaikannangit <128213310+vijaikannangit@users.noreply.github.com>
2024-08-28 16:52:46 +0530

    T2323 : Darker Test

--- lint-darker-test.py
+++ lint-darker-test.py
@@ -1,4 +1,4 @@
-import os, sys, datetime, date
+import os, sys, datetime, date, requests
 import re, csv, request
 
 def example_function():
@@ -15,7 +15,7 @@ def example_function():
 def another_function():
     print("Another function")
     if True:
-        print("This line is fine")
+        print("This line is fine hello")
     else:
       print("This line is not fine 123")
       print("Another line with trailing whitespace ")
