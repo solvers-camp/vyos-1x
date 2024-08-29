025372c8ccbbb350744de97056e5563a83b2f7c8
vijaikannangit <128213310+vijaikannangit@users.noreply.github.com>
2024-08-29 17:42:51 +0530

    T3002 : Darker-Test

--- sample1.py
+++ sample1.py
@@ -4,11 +4,13 @@ import datetime
 import date
 import csv
 import request
+import os
 
 def hello_world():
   print("Another line Sample with error")# No Space check
   print("Hello, world!")
   print("Hello, world1!")
+  print("Hello, world2!")
   print("Another line")# No Space check
   print("Another line 1")# No Space check
   print("Another line 2")# No Space check
