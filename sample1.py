e03a74d83a2e5b1acad62359d645d0607f37e96a
vijaikannangit <128213310+vijaikannangit@users.noreply.github.com>
2024-08-30 17:25:44 +0530

    T0051 :Darker-Test

--- sample1.py
+++ sample1.py
@@ -4,10 +4,11 @@ import response
 import csv
 
 def hello_world():
-  print("Hello, world!")
-  print("Another line")# No Space check
-  print("Another 1")# No Space check
-  print("Another 2")# No Space check
-  print("show settings 2")# No Space check
+          print("Hello, world!")
+          print("Another line")# No Space check
+          print("Another 1")# No Space check
+          print("Another 2")# No Space check
+          print("show settings 2")# No Space check
+          print("1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890")
 
 hello_world()
