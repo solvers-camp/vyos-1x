395b3405d14abf258cfb027d434ebd273e1490fd
vijaikannangit <128213310+vijaikannangit@users.noreply.github.com>
2024-09-02 14:51:41 +0530

    T7001 : Darker - Test

--- example1.py
+++ example1.py
@@ -1,5 +1,5 @@
 def example_function():
-    print("This")
+    print("This line exceeds the 30 character limit and should be flagged by the linter.")
     if True:
         print("This line is indented with 4 spaces vijai.")
     else:
