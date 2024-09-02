1f4de85777a26113b5b061438a30e9e385200655
vijaikannangit <128213310+vijaikannangit@users.noreply.github.com>
2024-09-02 12:07:32 +0530

    T4545 : Darker - Test

--- sample1.py
+++ sample1.py
@@ -1,6 +1,6 @@
 def example_function():
-    print("This line exceeds the 30 character limit and should be flagged by the linter testing ruff.toml.")
+    print("This line exceeds the 30 character limit and should be flagged by the linter testing ruff.toml")
     if True:
-        print("This line is indented with 4 spaces.")# Spacing Error
+        print("This line is indented with 4 spaces")# Spacing Error
     else:
         print("This line is also indented with 4 spaces.")
